da211837b6caa2d7988f93ff0cb32a8fa28a75ad
hellbovh <69521565+hellbovh@users.noreply.github.com>
2023-03-16 00:21:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,26 +2,26 @@
 
 int cbinsearch(int *arr, int size, int value) {
   int count = 0;
-	int left_border = 0, right_border = size - 1, mid = 0;
-	while (left_border <= right_border) {
-		mid = (right_border + left_border) / 2;
-		if (arr[mid] < value) {
-			left_border = mid + 1;
-		} else if (arr[mid] > value) {
-			right_border = mid - 1;
-		} else {
-			break;
-		}
-	}
-	int temp = mid;
-	while (arr[mid] == value) {
-		count++;
-		mid++;
-	}
-	mid = temp - 1;
-	while (arr[mid] == value) {
-		count++;
-		mid--;
-	}
-	return count;
+  int left_border = 0, right_border = size - 1, mid = 0;
+  while (left_border <= right_border) {
+    mid = (right_border + left_border) / 2;
+    if (arr[mid] < value) {
+      left_border = mid + 1;
+    } else if (arr[mid] > value) {
+      right_border = mid - 1;
+    } else {
+      break;
+    }
+  }
+  int temp = mid;
+  while (arr[mid] == value) {
+    count++;
+    mid++;
+  }
+  mid = temp - 1;
+  while (arr[mid] == value) {
+    count++;
+    mid--;
+  }
+  return count;
 }
